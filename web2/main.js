a38a8479c5c8eaaf10876124f60df0ec8bbefe22
jungheeeun <jungheeeun272@gmail.com>
2023-08-23 21:22:19 +0900

    글목록 추가하기

--- web2/main.js
+++ web2/main.js
@@ -4,25 +4,15 @@ var url = require('url');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
-  var queryData = url.parse(_url, true).query;//query: [Object: null prototype] { id: 'CSS' },
-  var pathname = url.parse(_url, true).pathname; //pathname: '/',
-  //console.log(url.parse(_url, true).pathname);
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
   if (pathname === '/') {
     if (queryData.id === undefined) {
 
       fs.readdir('./data', function (error, filelist) {
-        console.log(filelist);
 
         var title = "Welcome";
         var description = "Hello Node.js";
-
-        /*
-        var list = ` <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>`;
-        */
         var list = '<ul>';
 
         var i = 0;
@@ -30,7 +20,6 @@ var app = http.createServer(function (request, response) {
           list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
           i = i + 1;
         }
-
         list = list + '</ul>';
 
         var template = `
@@ -54,9 +43,22 @@ var app = http.createServer(function (request, response) {
 
 
     } else {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-        var title = queryData.id;
-        var template = `
+      fs.readdir('./data', function (error, filelist) {
+
+        var title = "Welcome";
+        var description = "Hello Node.js";
+        var list = '<ul>';
+
+        var i = 0;
+        while (i < filelist.length) {
+          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          i = i + 1;
+        }
+        list = list + '</ul>';
+
+        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+          var title = queryData.id;
+          var template = `
         <!doctype html>
       <html>
       <head>
@@ -65,18 +67,15 @@ var app = http.createServer(function (request, response) {
       </head>
       <body>
         <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
+          ${list}
         <h2>${title}</h2>
         <p>${description}</p>
       </body>
       </html>
         `
-        response.writeHead(200);
-        response.end(template);
+          response.writeHead(200);
+          response.end(template);
+        });
       });
     }
   } else {
