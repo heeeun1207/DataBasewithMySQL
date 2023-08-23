a9d8da905a1e40c521a2323d8097a60cdbe917c9
jungheeeun <jungheeeun272@gmail.com>
2023-08-23 21:12:14 +0900

    파일목록 확인하고 , 가져와서 사용하기

--- web2/main.js
+++ web2/main.js
@@ -9,9 +9,30 @@ var app = http.createServer(function (request, response) {
   //console.log(url.parse(_url, true).pathname);
   if (pathname === '/') {
     if (queryData.id === undefined) {
-      fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+
+      fs.readdir('./data', function (error, filelist) {
+        console.log(filelist);
+
         var title = "Welcome";
         var description = "Hello Node.js";
+
+        /*
+        var list = ` <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>`;
+        */
+        var list = '<ul>';
+
+        var i = 0;
+        while (i < filelist.length) {
+          list = list + `<li>${filelist[i]}</li>`
+          i = i + 1;
+        }
+
+        list = list + '</ul>';
+
         var template = `
         <!doctype html>
       <html>
@@ -21,11 +42,7 @@ var app = http.createServer(function (request, response) {
       </head>
       <body>
         <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
+        ${list}
         <h2>${title}</h2>
         <p>${description}</p>
       </body> 
@@ -33,7 +50,9 @@ var app = http.createServer(function (request, response) {
         `
         response.writeHead(200);
         response.end(template);
-      });
+      })
+
+
     } else {
       fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
         var title = queryData.id;
---
+++ web2/syntax/array.js
@@ -0,0 +1,6 @@
+//CRUD
+var arr = ['A', 'B', 'C', 'D'];
+console.log(arr[2]);
+arr[2] = 3;
+console.log(arr);
+console.log(arr.length);
